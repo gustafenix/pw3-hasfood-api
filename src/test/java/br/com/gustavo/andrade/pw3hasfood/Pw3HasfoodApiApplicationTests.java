90b1b7fc8f2640c26cb51ee0403ea4743ccd9cbc
gustavoa <email@test.com>
2022-03-28 07:55:33 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/gustavo/andrade/pw3hasfood/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.andrade.pw3hasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
