a30208f48b4bd200559370f32c47c2e22e874153
gustavoa <email@test.com>
2022-03-28 08:31:12 -0300

    Criacao da enum StatusEnum.

---
+++ src/main/java/br/com/etechoracio/hasfood/enums/StatusEnum.java
@@ -0,0 +1,8 @@
+package br.com.etechoracio.hasfood.enums;
+
+import org.omg.PortableInterceptor.ACTIVE;
+import org.omg.PortableInterceptor.INACTIVE;
+
+public enum StatusEnum implements INACTIVE, ACTIVE {
+
+}
